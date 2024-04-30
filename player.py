06cea7fe3bb2a5f709b5e57a71e6d5f7a40abf90
M4tthi4sk <148368101+M4tthi4sk@users.noreply.github.com>
2024-04-30 18:54:23 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,21 +1,22 @@
 import pygame
 
+
 class Player:
     def __init__(self, game, x, y):
         self.x = x
         self.y = y
         self.game = game
         self.surface = game.window
-        self.rect = pygame.Rect(self.x,self.y,64,64)
-    
+        self.rect = pygame.Rect(self.x, self.y, 64, 64)
+
     def update(self):
-        self.rect = pygame.Rect(self.x,self.y, 64,64)
+        self.rect = pygame.Rect(self.x, self.y, 64, 64)
         self.movement(500)
         self.draw()
-    
+
     def draw(self):
-        pygame.draw.rect(self.surface, "red", (self.x,self.y, 64,64))
-    
+        pygame.draw.rect(self.surface, "red", (self.x, self.y, 64, 64))
+
     def movement(self, speed):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
