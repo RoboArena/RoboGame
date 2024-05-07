592f81376b8712499919f014cc9f5d848294db7a
Amina <amina.siebert@student.uni-tuebingen.de>
2024-05-07 11:17:07 +0200

    Adding mouse oriented direction

--- player.py
+++ player.py
@@ -2,21 +2,24 @@ import pygame
 
 
 class Player:
-    def __init__(self, game, x, y, r):
+    def __init__(self, game, x, y):
         self.x = x
         self.y = y
-        self.r = r
+        self.r = 32
+        self.dir = (90, 90)
         self.game = game
         self.surface = game.window
-        self.rect = pygame.Rect(self.x, self.y, self.r, self.r)
 
     def update(self):
-        self.rect = pygame.Rect(self.x, self.y, self.r, self.r)
+        mouse_pos = pygame.mouse.get_pos()
+        self.dir = (self.x - mouse_pos[0], self.y - mouse_pos[1])
         self.movement(500)
         self.draw()
 
     def draw(self):
         pygame.draw.circle(self.surface, "blue", (self.x, self.y), self.r)
+        endOfLine = (self.x - self.dir[0], self.y - self.dir[1])
+        pygame.draw.line(self.surface, "black", (self.x, self.y), endOfLine)
 
     def movement(self, speed):
         keys = pygame.key.get_pressed()
